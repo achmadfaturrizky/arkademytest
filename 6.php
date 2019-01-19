98229f1417aec926978c1616699d965c10a2aea8
achmadfaturrizky <xrutafx@gmail.com>
2019-01-19 09:11:57 +0000

    bismillah semoga lolos

--- 6.php
+++ 6.php
@@ -2,8 +2,8 @@
 <title>Query Nurhadi No 6</title>
 <head>
 	<body>
-	 <p>"Query SQL Nurhadi - SELECT categories.id, categories.name AS name, GROUP_CONCAT(hobbies.name) AS person_hobbies
-FROM categories, hobbies WHERE hobbies.person_id = categories.id GROUP BY categories.id"</p>
+	 <p>"Query SQL Nurhadi - SELECT persons.id, persons.name AS name, GROUP_CONCAT(hobbies.name) AS person_hobbies
+FROM persons, hobbies WHERE hobbies.person_id = persons.id GROUP BY persons.id ORDER BY name ASC"</p>
 <br>
 <img src="ss_6.png" width="1000" height="500">
 </body>
