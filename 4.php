317f8dae715eb9d58bcf5c48856b593abd8b4e67
achmadfaturrizky <xrutafx@gmail.com>
2019-01-19 08:46:24 +0000

    bismillah semoga lolos

--- 4.php
+++ 4.php
@@ -1,5 +1,13 @@
 <?php
-$kelompok = "kelompok";
-$ganti_nama = str_replace("o", "a", $kelompok);
-echo $ganti_nama;
-?>
+function print_abc($col,$row){
+    $x = 0;
+    $abjad = range('a', 'z');
+    for ($i=0; $i < $row; $i++) { 
+        for ($j = 1; $j <= $col; $j++) {
+            echo $abjad[$x].'&nbsp;';  
+            $x++;
+        }
+        echo "<br>";
+    }
+}
+print_abc(3,4);
