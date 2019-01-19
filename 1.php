3159aeb24969cbe6a88cc6d77e006d39b0467814
achmadfaturrizky <xrutafx@gmail.com>
2019-01-19 08:30:27 +0000

    bismillah, semoga lolos

--- 1.php
+++ 1.php
@@ -1,46 +1,16 @@
 <?php
+function myBiodata()
+{
+    $biodata = [
+        'name' => 'Achmad Fatur Rizky',
+        'adderss' => 'Jl. Purwomukti Barat 1 no 07, Semarang, Jawa Tengah 50192',
+        'hobbies' => ['Coding', 'Nonton Film', 'Travelling', 'Touring'],
+        'is_married' => false,
+        'school' => ['highSchool' => 'SMK NEGERI 9 SEMARANG', 'university' => ''],
+        'skills' => ['skills' => ['PHP Native', 'JS']]
+    ];
 
-class MY_biodata {
-
-	private $_json;
-
-	public $_name;
-	public $_address;
-	public $_hobbies;
-	public $_is_married;
-	public $_school;
-	public $_skills;
-
-	public function getJSON() {
-
-		$this->_json = array(
-
-			'name'			=> $this->_name,
-			'address'		=> $this->_address,
-			'hobbies'		=> $this->_hobbies,
-			'is_married'	=> $this->_is_married,
-			'school'		=> (object) $this->_school,
-			'skills'		=> (object) $this->_skills,
-
-		);
-
-		return json_encode( $this->_json );
-
-	}
-
+    return print_r(json_encode($biodata, true));
 }
 
-$my_biodata 				= new MY_biodata();
-$my_biodata->_name 			= 'Achmad Fatur Rizky';
-$my_biodata->_address 		= 'Jl. Purwomukti Barat 1 No 07';
-$my_biodata->_hobbies		= array( 'Coding', 'Traveling' );
-$my_biodata->_is_married	= false;
-$my_biodata->_school		= array(
-
-	'highSchool'	=> 'SMK Negeri 9 Semarang',
-	'university'	=> 'unknown'		
-
-);
-$my_biodata->_skills		= array( 'Web Design' , 'UI/UX Design' , 'Web Programming', 'PHP Native' );
-
-echo $my_biodata->getJSON();
+myBiodata();
