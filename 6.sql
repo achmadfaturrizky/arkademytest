54300e715b6dd1d99098898e7112876fa147c4d5
achmadfaturrizky <xrutafx@gmail.com>
2019-01-19 08:10:08 +0000

    bismillah semoga lolos

--- 6.sql
+++ 6.sql
@@ -1,26 +1,40 @@
-CREATE DATABASE  IF NOT EXISTS `nurhadi`;
-USE `nurhadi`;
-CREATE TABLE `categories` (
-  `id` int(11) NOT NULL,
-  `name` varchar(225) NOT NULL
-);
-ALTER TABLE `categories` ADD PRIMARY KEY (`id`);
-ALTER TABLE `categories` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 19 Jan 2019 pada 09.01
+-- Versi Server: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
-INSERT INTO `categories` (`id`, `name`) VALUES
-(1, 'Nurhadi'),
-(2, 'Aldo'),
-(3, 'Dodi');
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nurhadi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `hobbies`
+--
 
 CREATE TABLE `hobbies` (
   `id` int(11) NOT NULL,
   `person_id` int(11) NOT NULL,
   `name` varchar(225) NOT NULL
-  
-);
-ALTER TABLE `hobbies` ADD PRIMARY KEY (`id`), ADD KEY `person_id` (`person_id`);
-ALTER TABLE `hobbies` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-ALTER TABLE `hobbies` ADD CONSTRAINT `hobbies_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `categories` (`id`);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `hobbies`
+--
 
 INSERT INTO `hobbies` (`id`, `person_id`, `name`) VALUES
 (1, 1, 'Bird watching'),
@@ -30,6 +44,69 @@ INSERT INTO `hobbies` (`id`, `person_id`, `name`) VALUES
 (5, 2, 'Beekeeping'),
 (6, 3, 'Book restoration');
 
+-- --------------------------------------------------------
 
-SELECT categories.id, categories.name AS name, GROUP_CONCAT(hobbies.name) AS person_hobbies
-FROM categories, hobbies WHERE hobbies.person_id = categories.id GROUP BY categories.id
+--
+-- Struktur dari tabel `persons`
+--
+
+CREATE TABLE `persons` (
+  `id` int(11) NOT NULL,
+  `name` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `persons`
+--
+
+INSERT INTO `persons` (`id`, `name`) VALUES
+(1, 'Nurhadi'),
+(2, 'Aldo'),
+(3, 'Dodi');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `hobbies`
+--
+ALTER TABLE `hobbies`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `person_id` (`person_id`);
+
+--
+-- Indexes for table `persons`
+--
+ALTER TABLE `persons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `hobbies`
+--
+ALTER TABLE `hobbies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `persons`
+--
+ALTER TABLE `persons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `hobbies`
+--
+ALTER TABLE `hobbies`
+  ADD CONSTRAINT `hobbies_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `persons` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SELECT persons.id, persons.name AS name, GROUP_CONCAT(hobbies.name) AS person_hobbies
+FROM persons, hobbies WHERE hobbies.person_id = persons.id GROUP BY persons.id
